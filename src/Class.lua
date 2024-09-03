3342b92369932722e57ba5105c38a9c294704c0d
RVCDv <101155145+lnconcinnity@users.noreply.github.com>
2024-09-03 16:31:38 +0800

    Update Class.lua

--- src/Class.lua
+++ src/Class.lua
@@ -6,7 +6,7 @@ local STRICTIFIY_VALUE_MARKER = newproxy()
 local INTERNAL_MARKER = newproxy()
 local PUBLIC_MARKER = newproxy()
 local LOCKED_MARKER = newproxy() -- cant change after runtime fr
-local C_HANDLERS_MARKER = newproxy()
+local SPECIAL_HANDLER_MARKER = newproxy()
 
 local EXPLICIT_PRIVATE_PREFIX = "_"
 local EXPLICIT_PROTECTED_PREFIX = "__"
@@ -20,7 +20,7 @@ local CANNOT_WRITE_CONSTANT = "Attempted to overwrite constant \"%s\""
 local CANNOT_WRITE_LOCKED = "Attempt to overwrite locked property \"%s\""
 
 local CLONE_IGNORE_PROPERTIES = {'new'}
-local AUTOLOCK_PROPERTIES = {'__wrapSignal__', '__lockProperty', '__unlockProperty', '__strictifyProperty__', '__canStrictifyProperties__', '__canMakeConstants__'}
+local AUTOLOCK_PROPERTIES = {'__wrapSignal', '__lockProperty', '__unlockProperty', '__strictifyProperty__', '__canStrictifyProperties__', '__canMakeConstants__', '__propChangedSignals__', '__wrapCoroutine', '__wrapTask', '__registerCHandler__'}
 
 local function hasFunction(class, method)
 	for _, fn in pairs(class) do
@@ -51,7 +51,7 @@ local function isWithinClassScope(class, self, includeInherited)
 		local _, methodName = pcall(debug.info, level, 'n')
 		if not method then break end
 
-		if hasFunction(class, method) or class[methodName] == method or rawget(self, C_HANDLERS_MARKER)[method] ~= nil then
+		if hasFunction(class, method) or class[methodName] == method or rawget(self, SPECIAL_HANDLER_MARKER)[method] ~= nil then
 			calledWithinFunction = true
 		end
 
@@ -95,7 +95,7 @@ local function initSelf(class, defaultProps)
 		[INTERNAL_MARKER] = {},
 		[STRICTIFIY_VALUE_MARKER] = {},
 		[PUBLIC_MARKER] = {},
-		[C_HANDLERS_MARKER] = {},
+		[SPECIAL_HANDLER_MARKER] = setmetatable({}, {__mode = 'k'}),
 	}, {}
 
 	local function insert(source)
@@ -150,16 +150,6 @@ local function Class(defaultProps: {}?)
 	end
 
 	function meta:__newindex(key, value)
-		local internal, private, protected, public = rawget(self, INTERNAL_MARKER),
-		rawget(self, PRIVATE_MARKER),
-		rawget(self, PROTECTED_MARKER),
-		rawget(self, PUBLIC_MARKER)
-
-		local oldValue = internal[key] or private[key] or protected[key] or public[key]
-		if oldValue == value then
-			return
-		end
-
 		local accessingPrivate, accessingProtected, accessingLocked, accessingInternal = isAccessingPrivate(key), isAccessingProtected(key), rawget(self, LOCKED_MARKER)[key:sub(2)] ~= nil, isAccessingInternal(key)
 		if accessingLocked then
 			error(string.format(
@@ -172,6 +162,16 @@ local function Class(defaultProps: {}?)
 				if accessingPrivate then WRITE_PRIVATE_NO_ACCESS else WRITE_PROTECTED_NO_ACCESS,
 				tostring(key), tostring(value)), 2)
 		else
+			local internal, private, protected, public = rawget(self, INTERNAL_MARKER),
+			rawget(self, PRIVATE_MARKER),
+			rawget(self, PROTECTED_MARKER),
+			rawget(self, PUBLIC_MARKER)
+	
+			local oldValue = internal[key] or private[key] or protected[key] or public[key]
+			if oldValue == value then
+				return
+			end
+
 			local predicate = rawget(self, STRICTIFIY_VALUE_MARKER)[key]
 			if predicate ~= nil then
 				local ok, err = predicate(value)
@@ -196,6 +196,14 @@ local function Class(defaultProps: {}?)
 				end
 				public[key] = value
 			end
+
+			local sigContainer = rawget(self, '__propChangedSignals__')
+			if sigContainer then
+				local foundChangedSignal = sigContainer[key] :: BindableEvent | nil
+				if foundChangedSignal then
+					foundChangedSignal:Fire(value, oldValue)
+				end
+			end
 		end
 	end
 
@@ -204,6 +212,7 @@ local function Class(defaultProps: {}?)
 		local self = setmetatable(markers, meta)
 		self.__canMakeConstants__ = true
 		self.__canStrictifyProperties__ = true
+		self.__propChangedSignals__ = {}
 
 		pasteSelf(self, realProps)
 		if self.__init then
@@ -242,26 +251,37 @@ local function Class(defaultProps: {}?)
 		return subClass
 	end
 
+	function class:OnPropertyChanged(propName: string, handler: (...any) -> ()): RBXScriptConnection
+		local signal = self.__propSignals__[propName]
+		if not signal then
+			signal = Instance.new("BindableEvent")
+			signal.Name = propName
+			self.__propSignals__[propName] = signal
+		end
+		local conn = self:__wrapSignal(signal.Event, handler)
+		return conn
+	end
+	
 	function class:__strictifyProperty__(propName: string, predicate: (value: any) -> boolean)
 		assert(self.__canStrictifyProperties__ == true, "Cannot strictify properties after initialization")
 		self[STRICTIFIY_VALUE_MARKER][propName] = predicate
 	end
 
-	function class:__registerCHandler__(handler)
-		self[C_HANDLERS_MARKER][handler] = true
+	function class:__registerSpecialHandler__(handler)
+		self[SPECIAL_HANDLER_MARKER][handler] = true
 		return handler
 	end
 	
 	function class:__wrapSignal(signal, handler)
-		return signal:Connect(self:__registerCHandler__(handler))
+		return signal:Connect(self:__registerSpecialHandler__(handler))
 	end
 	
 	function class:__wrapCoroutine(coroutine, handler)
-		return coroutine(self:__registerCHandler__(handler))
+		return coroutine(self:__registerSpecialHandler__(handler))
 	end
 	
 	function class:__wrapTask(task, handler)
-		return task(self:__registerCHandler__(handler))
+		return task(self:__registerSpecialHandler__(handler))
 	end
 
 	function class:__lockProperty(propName: string)
